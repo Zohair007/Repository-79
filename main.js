a0141f7db7ed73f885bc1c3bb11db379ce4b1a7a
Zohair007 <89848636+Zohair007@users.noreply.github.com>
2022-01-20 15:21:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ menu_list_array = ["Veg Margherita Pizza",
     
     function getmenu(){
         var htmldata="";
-        htmldata="col class='menulist'>"
+        htmldata="<ol class='menulist'>"
         menu_list_array.sort();
         for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+'<li>'+menu_list_array[i] + '<br>'
+            htmldata=htmldata+'<li>'+menu_list_array[i] + '</li>'
         }
         htmldata=htmldata+"</ol>"
         document.getElementById("display_menu").innerHTML = htmldata;
@@ -24,10 +24,11 @@ menu_list_array = ["Veg Margherita Pizza",
         menu_list_array.push(item);
         menu_list_array.sort();
         // use the sort function as - menu_list_array.sort();
-        htmldata="<section class="cards">";
+        htmldata="<section class='cards'>";
         for(var i=0;i<menu_list_array.length;i++){
-              menu_list_array[i]= '<br>'
+        htmldata=htmldata+'<div class="card">'+ '<img id="im1"src="pizzaImg.png"/>'+ menu_list_array[i] + '</div>'
         }    
+        htmldata=htmldata+"</section>"
         document.getElementById("display_addedmenu").innerHTML = htmldata;
      }
 function add_top(){
